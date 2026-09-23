724c7f020780924b1efd452de2ebf259f4305436
Divya Dhumane <dhumanedivya71@gmail.com>
2026-09-23 18:41:09 +0530

    Add Shape, Rectangle, and Circle classes
    Implemented Shape class hierarchy with area calculation.

---
+++ Base_Reference_With_Virtual_Function.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual double area() const {
+        return 0.0;
+    }
+
+    virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double givenLength, double givenWidth) {
+        length = givenLength;
+        width = givenWidth;
+    }
+
+    double area() const override {
+        return length * width;
+    }
+};
+
+class Circle : public Shape {
+private:
+    double radius;
+
+public:
+    explicit Circle(double givenRadius) {
+        radius = givenRadius;
+    }
+
+    double area() const override {
+        const double PI = 3.141592653589793;
+        return PI * radius * radius;
+    }
+};
+
+void printArea(const Shape& shape) {
+    cout << "Area: " << shape.area() << endl;
+}
+
+int main() {
+    Rectangle rectangle(5.0, 3.0);
+    Circle circle(2.0);
+
+    printArea(rectangle);
+    printArea(circle);
+
+    return 0;
+}
