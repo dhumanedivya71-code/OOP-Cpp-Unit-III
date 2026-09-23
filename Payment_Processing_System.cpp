e4d61053552bd53c2b34ef80fa70322927c259da
Divya Dhumane <dhumanedivya71@gmail.com>
2026-09-23 19:32:52 +0530

    Create Payment_Processing_System.cpp

---
+++ Payment_Processing_System.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Payment {
+public:
+    virtual void pay(double amount) const = 0;
+
+    virtual ~Payment() = default;
+};
+
+class CardPayment : public Payment {
+public:
+    void pay(double amount) const override {
+        cout << "Paid Rs. " << amount
+             << " using card" << endl;
+    }
+};
+
+class UpiPayment : public Payment {
+public:
+    void pay(double amount) const override {
+        cout << "Paid Rs. " << amount
+             << " using UPI" << endl;
+    }
+};
+
+class NetBankingPayment : public Payment {
+public:
+    void pay(double amount) const override {
+        cout << "Paid Rs. " << amount
+             << " using net banking" << endl;
+    }
+};
+
+void processPayment(const Payment& payment, double amount) {
+    payment.pay(amount);
+}
+
+int main() {
+    CardPayment card;
+    UpiPayment upi;
+    NetBankingPayment netBanking;
+
+    processPayment(card, 1250.0);
+    processPayment(upi, 750.0);
+    processPayment(netBanking, 500.0);
+
+    return 0;
+}
